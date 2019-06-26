a069031f86227f7ce7f50c888153048edf62ea6e
rodrigobass133 <rodrigobass133@gmail.com>
2019-06-26 18:58:41 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>UCB UNIVERSE</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="background">
+    <div class="font-main">
+        <div class="text-center">
+            <!--<h1 class="mt-6">UCB <p>UNIVERSE</p></h1>-->
+            <div class="mt-6">
+                <img src="pix/title.png" alt="some text">
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+            <div class="col-6, mt-6">
+                <div class="f-black">
+                    <div class="card">
+                        <div class="card-header">
+                            Bem vindo ao UCB UNIVERSE!!!
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title"></h5>
+                            <p class="card-text">Vamos decolar nessa aventura.</p>
+                            <button href="/proxTela.php" class="flash button">Press Start!</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
